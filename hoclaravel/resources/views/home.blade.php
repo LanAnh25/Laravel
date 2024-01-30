a7a776df1f070a6c030ce16ea83f8df245d449b5
LanAnh <anh.pham25@student.passerellesnumeriques.org>
2024-01-30 22:30:16 +0700

    bai3

--- hoclaravel/resources/views/home.blade.php
+++ hoclaravel/resources/views/home.blade.php
@@ -1,1 +1,11 @@
 <h1 style="text-align: center;">Học Laravel tại Unicode</h1>
+<?php 
+if(env('APP_ENV')=='production'){
+    //Call api live
+    echo 'Call api live';
+}else {
+    //Call api sandbox
+    echo 'Call api sandbox';
+}
+
+?>
